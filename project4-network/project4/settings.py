c5be08a235f95fcaf9661db63a7ca6303bd24ec6
wojwoj8 <wojwoj8@gmail.com>
2023-09-08 16:42:45 +0200

    for deploy

--- project4-network/project4/settings.py
+++ project4-network/project4/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -100,6 +100,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+import os
 
 
 # Internationalization
